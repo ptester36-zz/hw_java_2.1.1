0f95cb97d4c774c3b187c603fad42d94ee8c884f
Alexandr Polezhaev <alexsandr168@gmail.com>
2020-07-02 21:42:27 +0300

    add Main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiketPrice = 12_450;
+        int bonusMiles = tiketPrice / 20;
+        System.out.println("Начислено бонусных миль: " + bonusMiles);
+    }
+}
